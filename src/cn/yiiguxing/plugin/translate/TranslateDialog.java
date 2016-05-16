b9668377e8799e66ba1a8121b898aeda91b4606a
Yii.Guxing <yii.guxing@gmail.com>
2016-05-17 00:03:08 +0800

    添加历史记录

--- src/cn/yiiguxing/plugin/translate/TranslateDialog.java
+++ src/cn/yiiguxing/plugin/translate/TranslateDialog.java
@@ -2,20 +2,23 @@ package cn.yiiguxing.plugin.translate;
 
 
 import com.intellij.openapi.ide.CopyPasteManager;
+import com.intellij.openapi.ui.FixedComboBoxEditor;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.ui.DocumentAdapter;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.PopupMenuListenerAdapter;
+import com.intellij.ui.*;
+import com.intellij.ui.components.JBTextField;
+import org.jdesktop.swingx.autocomplete.AutoCompleteComboBoxEditor;
+import org.jdesktop.swingx.autocomplete.ObjectToStringConverter;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.PopupMenuEvent;
+import javax.swing.plaf.basic.BasicComboBoxEditor;
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 
@@ -24,19 +27,21 @@ public class TranslateDialog extends JDialog {
     private static final JBColor MSG_FOREGROUND = new JBColor(new Color(0xFF333333), new Color(0xFFBBBBBB));
     private static final JBColor MSG_FOREGROUND_ERROR = new JBColor(new Color(0xFF333333), new Color(0xFFEE0000));
 
+    private static final int MAX_HISTORIES_SIZE = 20;
+    private static final DefaultComboBoxModel<String> COMBO_BOX_MODEL = new DefaultComboBoxModel<>();
+
     private JPanel contentPane;
-    private JTextField queryText;
     private JButton queryBtn;
     private JLabel messageLabel;
     private JPanel msgPanel;
     private JTextPane resultText;
     private JScrollPane scrollPane;
+    private JComboBox<String> queryComboBox;
 
     private String currentQuery;
 
     public TranslateDialog() {
         setTitle("Translate");
-        setIconImage(Toolkit.getDefaultToolkit ().getImage("/icon_16.png"));
         setMinimumSize(new Dimension(400, 450));
         setModal(true);
         setLocationRelativeTo(null);
@@ -58,13 +63,10 @@ public class TranslateDialog extends JDialog {
             }
         });
 
-        queryBtn.addActionListener(e -> onQuery());
-        queryText.getDocument().addDocumentListener(new DocumentAdapter() {
-            @Override
-            protected void textChanged(DocumentEvent e) {
-                queryBtn.setEnabled(!Utils.isEmptyString(queryText.getText()));
-            }
-        });
+        queryBtn.setEnabled(false);
+        queryBtn.addActionListener(e -> query(queryComboBox.getEditor().getItem().toString()));
+
+        initQueryComboBox();
 
         rootPane.setDefaultButton(queryBtn);
         scrollPane.setVerticalScrollBar(scrollPane.createVerticalScrollBar());
@@ -76,6 +78,45 @@ public class TranslateDialog extends JDialog {
         scrollPane.setBackground(background);
 
         setComponentPopupMenu();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initQueryComboBox() {
+        queryComboBox.setModel(COMBO_BOX_MODEL);
+
+        JTextField field = (JTextField) queryComboBox.getEditor().getEditorComponent();
+        field.getDocument().addDocumentListener(new DocumentAdapter() {
+            @Override
+            protected void textChanged(DocumentEvent documentEvent) {
+                updateQueryButton();
+            }
+        });
+        field.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                field.setSelectionStart(0);
+                field.setSelectionEnd(field.getText().length());
+            }
+        });
+
+        queryComboBox.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                updateQueryButton();
+                onQuery();
+            }
+        });
+        queryComboBox.setRenderer(new ListCellRendererWrapper<String>() {
+
+            @Override
+            public void customize(JList list, String value, int index, boolean isSelected, boolean cellHasFocus) {
+                if (value != null && value.trim().length() > 20) {
+                    String trim = value.trim();
+                    setText(trim.substring(0, 15) + "..." + trim.substring(trim.length() - 3));
+                } else {
+                    setText(value == null ? "" : value.trim());
+                }
+            }
+        });
     }
 
     private void setComponentPopupMenu() {
@@ -117,15 +158,31 @@ public class TranslateDialog extends JDialog {
 
     public void query(String query) {
         if (Utils.isEmptyString(query)) {
-            query = "";
+            if (COMBO_BOX_MODEL.getSize() > 0) {
+                updateQueryButton();
+            }
+
+            return;
         }
-        queryText.setText(query);
+
         resultText.setText("");
-        onQuery();
+
+        queryComboBox.getEditor().setItem(query);
+        COMBO_BOX_MODEL.removeElement(query);
+        COMBO_BOX_MODEL.insertElementAt(query, 0);
+        queryComboBox.setSelectedIndex(0);
+
+        if (COMBO_BOX_MODEL.getSize() > MAX_HISTORIES_SIZE) {
+            COMBO_BOX_MODEL.removeElementAt(MAX_HISTORIES_SIZE);
+        }
     }
 
-    private void onQuery() {
-        currentQuery = queryText.getText();
+    private void updateQueryButton() {
+        queryBtn.setEnabled(!Utils.isEmptyString(queryComboBox.getEditor().getItem().toString()));
+    }
+
+    void onQuery() {
+        currentQuery = queryComboBox.getSelectedItem().toString();
         if (!Utils.isEmptyString(currentQuery)) {
             currentQuery = currentQuery.trim();
             messageLabel.setForeground(MSG_FOREGROUND);
