59982b3005fb37085095556b4b6fe368456fc812
Yii.Guxing <yii.guxing@gmail.com>
2016-10-11 17:34:06 +0800

    Settings

--- src/cn/yiiguxing/plugin/translate/Settings.java
+++ src/cn/yiiguxing/plugin/translate/Settings.java
@@ -4,21 +4,18 @@ import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Settings
  */
 @State(name = "TranslationSettings", storages = @Storage(id = "other", file = "$APP_CONFIG$/translation.xml"))
-public class Settings implements PersistentStateComponent<Settings.State> {
+public class Settings implements PersistentStateComponent<Settings> {
 
-    static class State {
-        public boolean useDefaultKey = true;
-        public String apiKeyName;
-        public String apiKeyValue;
-    }
-
-    private State state;
+    private boolean useDefaultKey = true;
+    private String apiKeyName;
+    private String apiKeyValue;
 
     /**
      * Get the instance of this service.
@@ -31,37 +28,46 @@ public class Settings implements PersistentStateComponent<Settings.State> {
 
     @Nullable
     @Override
-    public State getState() {
-        return state;
+    public Settings getState() {
+        return this;
     }
 
     @Override
-    public void loadState(State state) {
-        this.state = state;
+    public void loadState(Settings state) {
+        XmlSerializerUtil.copyBean(state, this);
     }
 
+    /**
+     * 返回是否是使用默认的API KEY.
+     */
     public boolean isUseDefaultKey() {
-        return state.useDefaultKey;
+        return useDefaultKey;
     }
 
+    /**
+     * 设置是否使用默认的API KEY.
+     */
     public void setUseDefaultKey(boolean useDefaultKey) {
-        state.useDefaultKey = useDefaultKey;
+        this.useDefaultKey = useDefaultKey;
     }
 
+    /**
+     * 返回API KEY name.
+     */
     public String getApiKeyName() {
-        return state.apiKeyName;
+        return this.apiKeyName;
     }
 
     public void setApiKeyName(String apiKeyName) {
-        state.apiKeyName = apiKeyName;
+        this.apiKeyName = apiKeyName;
     }
 
     public String getApiKeyValue() {
-        return state.apiKeyValue;
+        return this.apiKeyValue;
     }
 
     public void setApiKeyValue(String apiKeyValue) {
-        state.apiKeyValue = apiKeyValue;
+        this.apiKeyValue = apiKeyValue;
     }
 
 }
