c0cb9faaa6d02c9085a06ba8350e88ae0631c880
Yii.Guxing <yii.guxing@gmail.com>
2017-11-11 14:29:03 +0800

    fixed size card layout

--- src/main/kotlin/cn/yiiguxing/plugin/translate/ui/FixedSizeCardLayout.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/ui/FixedSizeCardLayout.kt
@@ -1,6 +1,7 @@
 package cn.yiiguxing.plugin.translate.ui
 
 import java.awt.CardLayout
+import java.awt.Component
 import java.awt.Container
 import java.awt.Dimension
 
@@ -10,41 +11,26 @@ import java.awt.Dimension
  * Created by Yii.Guxing on 2017/11/8
  */
 class FixedSizeCardLayout : CardLayout() {
-    override fun preferredLayoutSize(parent: Container): Dimension {
-        synchronized(parent.treeLock) {
-            val insets = parent.insets
+    override fun preferredLayoutSize(parent: Container): Dimension = parent.getSize { preferredSize }
+
+    override fun minimumLayoutSize(parent: Container): Dimension = parent.getSize { minimumSize }
+
+    private inline fun Container.getSize(targetSize: Component.() -> Dimension): Dimension {
+        synchronized(treeLock) {
+            val insets = insets
             var w = 0
             var h = 0
 
-            for (i in 0 until parent.componentCount) {
-                val comp = parent.getComponent(i)
+            for (i in 0 until componentCount) {
+                val comp = getComponent(i)
                 if (comp.isVisible) {
-                    val d = comp.preferredSize
+                    val d = comp.targetSize()
                     w = d.width
                     h = d.height
                     break
                 }
             }
-            return Dimension(insets.left + insets.right + w + hgap * 2,
-                    insets.top + insets.bottom + h + vgap * 2)
-        }
-    }
 
-    override fun minimumLayoutSize(parent: Container): Dimension {
-        synchronized(parent.treeLock) {
-            val insets = parent.insets
-            var w = 0
-            var h = 0
-
-            for (i in 0 until parent.componentCount) {
-                val comp = parent.getComponent(i)
-                if (comp.isVisible) {
-                    val d = comp.minimumSize
-                    w = d.width
-                    h = d.height
-                    break
-                }
-            }
             return Dimension(insets.left + insets.right + w + hgap * 2,
                     insets.top + insets.bottom + h + vgap * 2)
         }
