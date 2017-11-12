f7ce590404a9aaa8839623c6e7dd14a6d6cf3bab
Yii.Guxing <yii.guxing@gmail.com>
2017-11-12 20:17:03 +0800

    删除tts测试代码

--- src/main/kotlin/cn/yiiguxing/plugin/translate/action/ShowTranslationDialogAction.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/action/ShowTranslationDialogAction.kt
@@ -1,6 +1,6 @@
 package cn.yiiguxing.plugin.translate.action
 
-import cn.yiiguxing.plugin.translate.tts.TextToSpeech
+import cn.yiiguxing.plugin.translate.TranslationManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
@@ -21,7 +21,6 @@ class ShowTranslationDialogAction : AnAction(), DumbAware {
         if (ApplicationManager.getApplication().isHeadlessEnvironment)
             return
 
-        //TranslationManager.instance.showDialog(e.project)
-        TextToSpeech.INSTANCE.speak("Almost a decade ago, GitHub was created as a place for developers to work together on code. Now, millions of people around the world use our platform to build businesses, learn from each other, and create tools we’ll use for decades to come. Together, you’ve shown that some of the most inventive, impactful things happen when curious and creative people have a space to work together. Today, at GitHub Universe, we shared plans to build on our ten years of experience and 1.5 billion commits. We've taken the first step toward using the world's largest collection of open source data to improve the way we collaborate with these new experiences.")
+        TranslationManager.instance.showDialog(e.project)
     }
 }
