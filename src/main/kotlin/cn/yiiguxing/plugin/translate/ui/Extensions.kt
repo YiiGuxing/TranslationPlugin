8bcfa180350cefad637ee3ebe36cae5a89b2bda5
Yii.Guxing <yii.guxing@gmail.com>
2017-11-24 17:55:25 +0800

    TextComponent 大小自适应

--- src/main/kotlin/cn/yiiguxing/plugin/translate/ui/Extensions.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/ui/Extensions.kt
@@ -5,7 +5,38 @@
  */
 package cn.yiiguxing.plugin.translate.ui
 
+import sun.swing.SwingUtilities2
+import java.awt.Dimension
 import javax.swing.JComboBox
+import javax.swing.text.JTextComponent
 
 
+/**
+ * 当前选中项
+ */
 val <E> JComboBox<E>.selected: E get() = getItemAt(selectedIndex)
+
+/**
+ * 自适应大小
+ *
+ * @param maxWidth 最大宽度
+ */
+fun JTextComponent.adjustSize(maxWidth: Int = Int.MAX_VALUE) {
+    if (maxWidth < 0) {
+        throw IllegalArgumentException("maxWidth must be greater than 0.")
+    }
+
+    val minSize = if (isMinimumSizeSet) minimumSize else Dimension(0, 0)
+
+    size = Dimension(maxWidth, Int.MAX_VALUE)
+
+    val textWidth = SwingUtilities2.stringWidth(this, getFontMetrics(font), text)
+    val preSize = Dimension(preferredSize).apply {
+        width = minOf(width, textWidth)
+    }
+
+
+    size = Dimension(preSize)
+    minimumSize = minSize
+    preferredSize = preSize
+}
