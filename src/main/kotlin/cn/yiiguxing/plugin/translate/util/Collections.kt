9def13ac259712805971ce52467ab23a0c0028b4
Yii.Guxing <yii.guxing@gmail.com>
2017-12-05 11:25:46 +0800

    doc

--- src/main/kotlin/cn/yiiguxing/plugin/translate/util/Collections.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/util/Collections.kt
@@ -7,7 +7,9 @@ package cn.yiiguxing.plugin.translate.util
 
 import java.util.*
 
-
+/**
+ * Trims the [MutableList] to [maxSize]
+ */
 fun <T> MutableList<T>.trimToSize(maxSize: Int): Boolean {
     var size = this.size
     val trim = size > 0 && size > maxSize
@@ -19,8 +21,14 @@ fun <T> MutableList<T>.trimToSize(maxSize: Int): Boolean {
     return trim
 }
 
+/**
+ * Returns a read list for JVM.
+ */
 fun <T> List<T>.toJVMReadOnlyList(): List<T> = Collections.unmodifiableList(this)
 
+/**
+ * Creates an [Enumeration] for an [Iterator].
+ */
 fun <T> Iterable<T>.enumeration(): Enumeration<T> = object : Enumeration<T> {
     private val iterator = this@enumeration.iterator()
     override fun hasMoreElements(): Boolean = iterator.hasNext()
