89e564fe65d5041a2e0978c6b9a00d34d14ffdd1
Yii.Guxing <yii.guxing@gmail.com>
2017-12-25 12:04:52 +0800

    Update FixedSizeCardLayout

--- src/main/kotlin/cn/yiiguxing/plugin/translate/ui/FixedSizeCardLayout.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/ui/FixedSizeCardLayout.kt
@@ -11,28 +11,31 @@ import java.awt.Dimension
  * Created by Yii.Guxing on 2017/11/8
  */
 class FixedSizeCardLayout : CardLayout() {
-    override fun preferredLayoutSize(parent: Container): Dimension = parent.getSize { preferredSize }
 
+    override fun preferredLayoutSize(parent: Container): Dimension = parent.getSize { preferredSize }
     override fun minimumLayoutSize(parent: Container): Dimension = parent.getSize { minimumSize }
+    override fun maximumLayoutSize(parent: Container): Dimension = parent.getSize { maximumSize }
 
     private inline fun Container.getSize(targetSize: Component.() -> Dimension): Dimension {
         synchronized(treeLock) {
-            val insets = insets
             var w = 0
             var h = 0
 
             for (i in 0 until componentCount) {
                 val comp = getComponent(i)
                 if (comp.isVisible) {
-                    val d = comp.targetSize()
-                    w = d.width
-                    h = d.height
+                    comp.targetSize().let { (tw, th) ->
+                        w = tw
+                        h = th
+                    }
                     break
                 }
             }
 
-            return Dimension(insets.left + insets.right + w + hgap * 2,
-                    insets.top + insets.bottom + h + vgap * 2)
+            return with(insets) {
+                Dimension(left + right + w + hgap * 2, top + bottom + h + vgap * 2)
+            }
         }
     }
+
 }
