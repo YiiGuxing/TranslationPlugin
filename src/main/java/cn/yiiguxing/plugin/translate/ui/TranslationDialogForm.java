d3535b571dac4a7eaff674d94683efab6bafa6a5
Yii.Guxing <yii.guxing@gmail.com>
2017-12-28 22:56:15 +0800

    TranslationDialogForm

--- src/main/java/cn/yiiguxing/plugin/translate/ui/TranslationDialogForm.java
+++ src/main/java/cn/yiiguxing/plugin/translate/ui/TranslationDialogForm.java
@@ -2,6 +2,7 @@ package cn.yiiguxing.plugin.translate.ui;
 
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.ui.TitlePanel;
+import com.intellij.ui.components.labels.LinkLabel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -17,7 +18,12 @@ public class TranslationDialogForm {
     private TitlePanel mTitlePanel;
     private ComboBox<String> mInputComboBox;
     private JButton mTranslateButton;
-    private JPanel mContentPanel;
+    private JPanel mMainContentPanel;
+    private JPanel mLanguagePanel;
+    private ComboBox mTargetLangComboBox;
+    private LinkLabel mSwitchButton;
+    private ComboBox mSourceLangComboBox;
+    private JPanel mContentContainer;
     private JEditorPane mMessage;
 
     @NotNull
@@ -41,8 +47,33 @@ public class TranslationDialogForm {
     }
 
     @NotNull
-    public final JPanel getContentPanel() {
-        return mContentPanel;
+    public JPanel getMainContentPanel() {
+        return mMainContentPanel;
+    }
+
+    @NotNull
+    public JPanel getLanguagePanel() {
+        return mLanguagePanel;
+    }
+
+    @NotNull
+    public ComboBox getTargetLangComboBox() {
+        return mTargetLangComboBox;
+    }
+
+    @NotNull
+    public LinkLabel getSwitchButton() {
+        return mSwitchButton;
+    }
+
+    @NotNull
+    public ComboBox getSourceLangComboBox() {
+        return mSourceLangComboBox;
+    }
+
+    @NotNull
+    public final JPanel getContentContainer() {
+        return mContentContainer;
     }
 
     @NotNull
