0d0344fad8b226bc309a0cbded9b2903ea6f45ca
Yii.Guxing <yii.guxing@gmail.com>
2018-06-10 15:12:48 +0800

    Add instant translate dialog form

---
+++ src/main/java/cn/yiiguxing/plugin/translate/ui/form/InstantTranslateDialogForm.java
@@ -0,0 +1,15 @@
+package cn.yiiguxing.plugin.translate.ui.form;
+
+import com.intellij.openapi.ui.ComboBox;
+
+import javax.swing.*;
+
+public class InstantTranslateDialogForm {
+    private JPanel mRoot;
+    private ComboBox mSourceLangComboBox;
+    private ComboBox mTargetLangComboBox;
+    private JButton mSwapButton;
+    private JButton mTranslateButton;
+    private JTextArea mInputTextArea;
+    private JTextArea mTranslationTextArea;
+}
