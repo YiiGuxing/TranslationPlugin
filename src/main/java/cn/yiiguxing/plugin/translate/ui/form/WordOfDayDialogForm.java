76ef2fc8c7b8d1495bbbf0338ad0a0237d60d2db
Yii.Guxing <yii.guxing@gmail.com>
2019-08-21 10:37:01 +0800

    Update "word of the day" dialog form

--- src/main/java/cn/yiiguxing/plugin/translate/ui/form/WordOfDayDialogForm.java
+++ src/main/java/cn/yiiguxing/plugin/translate/ui/form/WordOfDayDialogForm.java
@@ -1,5 +1,11 @@
 package cn.yiiguxing.plugin.translate.ui.form;
 
+import cn.yiiguxing.plugin.translate.ui.TTSButton;
+import cn.yiiguxing.plugin.translate.ui.Viewer;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jetbrains.annotations.Nullable;
+
 import javax.swing.*;
 
 /**
@@ -7,8 +13,29 @@ import javax.swing.*;
  * <p>
  * Created by Yii.Guxing on 2019/08/19.
  */
-public class WordOfDayDialogForm {
-    protected JPanel rootPanel;
-    protected JButton nextWordButton;
-    protected JButton closeButton;
+public class WordOfDayDialogForm extends DialogWrapper {
+
+    private JPanel rootPanel;
+    protected Viewer wordView;
+    protected JLabel phoneticLabel;
+    protected TTSButton ttsButton;
+    protected Viewer explainsView;
+    protected JButton showExplainsButton;
+
+    protected WordOfDayDialogForm(@Nullable Project project) {
+        super(project);
+    }
+
+    private void createUIComponents() {
+        wordView = new Viewer();
+        explainsView = new Viewer();
+        ttsButton = new TTSButton();
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return rootPanel;
+    }
+
 }
---
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/ui/WordOfDayDialog.kt
@@ -0,0 +1,17 @@
+package cn.yiiguxing.plugin.translate.ui
+
+import cn.yiiguxing.plugin.translate.ui.form.WordOfDayDialogForm
+import com.intellij.openapi.project.Project
+
+/**
+ * WordOfDayDialog
+ *
+ * Created by Yii.Guxing on 2019/08/20.
+ */
+class WordOfDayDialog(project: Project?) : WordOfDayDialogForm(project) {
+
+    init {
+        init()
+    }
+
+}
