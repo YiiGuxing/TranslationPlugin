e2cae489f8bc3e9aaaf6f8bc17581e303a9fd50b
Yii.Guxing <yii.guxing@gmail.com>
2019-09-25 17:22:21 +0800

    Fixed a crash when switching translation engines in Android Studio
    issue: #343

---
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/action/TranslatorAction.kt
@@ -0,0 +1,30 @@
+package cn.yiiguxing.plugin.translate.action
+
+import cn.yiiguxing.plugin.translate.trans.Translator
+import cn.yiiguxing.plugin.translate.util.Settings
+import cn.yiiguxing.plugin.translate.util.TranslateService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.util.Condition
+
+/**
+ * TranslatorAction
+ *
+ * Created by Yii.Guxing on 2019/09/25.
+ */
+class TranslatorAction(private val translator: Translator) :
+    DumbAwareAction(translator.name, null, translator.icon) {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        Settings.translator = translator.id
+    }
+
+    companion object {
+        val ACTIONS: List<TranslatorAction> = TranslateService.getTranslators().map { TranslatorAction(it) }
+
+        val PRESELECT_CONDITION: Condition<AnAction> = Condition { action ->
+            (action as? TranslatorAction)?.translator?.id == Settings.translator
+        }
+    }
+}
