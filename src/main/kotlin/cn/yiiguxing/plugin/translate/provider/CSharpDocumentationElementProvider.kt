08c39284e3f4d303ae0ed22e27173376e8803036
Yii.Guxing <yii.guxing@gmail.com>
2020-01-03 10:10:53 +0800

    Rider compatible
    feature: #424

---
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/provider/CSharpDocumentationElementProvider.kt
@@ -0,0 +1,11 @@
+package cn.yiiguxing.plugin.translate.provider
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+
+class CSharpDocumentationElementProvider : DocumentationElementProvider {
+
+    override fun findDocumentationElementAt(psiFile: PsiFile, offset: Int): PsiElement? {
+        TODO("not implemented")
+    }
+}
