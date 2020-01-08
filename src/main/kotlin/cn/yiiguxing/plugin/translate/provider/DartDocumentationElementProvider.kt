065e0e01e33d4cc61d7a57e14d1c54a945dac0ce
Yii.Guxing <yii.guxing@gmail.com>
2020-01-08 16:12:40 +0800

    Optimize DartDocumentationElementProvider

--- src/main/kotlin/cn/yiiguxing/plugin/translate/provider/DartDocumentationElementProvider.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/provider/DartDocumentationElementProvider.kt
@@ -1,17 +1,18 @@
 package cn.yiiguxing.plugin.translate.provider
 
 import cn.yiiguxing.plugin.translate.util.elementType
-import cn.yiiguxing.plugin.translate.util.findChild
+import cn.yiiguxing.plugin.translate.util.findChildOfType
 import cn.yiiguxing.plugin.translate.util.getNextSiblingSkippingCondition
 import cn.yiiguxing.plugin.translate.util.getPrevSiblingSkippingCondition
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiWhiteSpace
-import com.jetbrains.lang.dart.DartTokenTypes
 import com.jetbrains.lang.dart.DartTokenTypesSets
+import com.jetbrains.lang.dart.psi.DartComponent
 import com.jetbrains.lang.dart.psi.DartComponentName
 import com.jetbrains.lang.dart.psi.DartDocComment
+import com.jetbrains.lang.dart.psi.DartVarDeclarationList
 
 class DartDocumentationElementProvider : DocumentationElementProvider {
 
@@ -51,20 +52,16 @@ class DartDocumentationElementProvider : DocumentationElementProvider {
         private val PsiComment.owner: PsiElement?
             get() {
                 // 文档注释类型中，多行注释有最高的优先级。
-                // 如果当前注释不是多行注释(DartDocComment)，
-                // 则向上寻找，如果上方有多行注释，则说明当前的注释是无效的。
+                // 如果当前注释不是多行注释(DartDocComment)，则向上寻找，如果上方有多行注释，则说明当前的注释是无效的。
+                // 且，最下方的多行文档注释有最高的优先级，向下寻找时如遇文档注释，则当前的注释也是无效的。
+
                 if (this !is DartDocComment && !checkPreviousComments()) {
                     return null
                 }
 
-                val sibling = getNextSiblingSkippingCondition(SKIPPING_CONDITION)
-                    ?.takeUnless { it is DartDocComment } // 最下方的多行文档注释有最高的优先级，如遇文档注释，则当前的注释是无效的。
-                    ?: return null
-                return when (sibling.elementType) {
-                    DartTokenTypes.CLASS_DEFINITION,
-                    DartTokenTypes.FUNCTION_DECLARATION_WITH_BODY,
-                    DartTokenTypes.FUNCTION_DECLARATION_WITH_BODY_OR_NATIVE -> sibling
-                    DartTokenTypes.VAR_DECLARATION_LIST -> sibling.findChild(condition = DART_COMPONENT_NAME_CONDITION)
+                return when (val sibling = getNextSiblingSkippingCondition(SKIPPING_CONDITION)) {
+                    is DartComponent -> sibling.componentName
+                    is DartVarDeclarationList -> sibling.findChildOfType(DartComponent::class.java)?.componentName
                     else -> null
                 }
             }
