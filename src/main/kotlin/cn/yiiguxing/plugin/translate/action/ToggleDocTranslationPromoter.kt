19bba8e5d9dcdaf9af1030933e29ad2341ec9f54
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-05-22 14:28:19 +0300

    add action to toggle quick documentation translation

---
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/action/ToggleDocTranslationPromoter.kt
@@ -0,0 +1,20 @@
+package cn.yiiguxing.plugin.translate.action
+
+import com.intellij.openapi.actionSystem.ActionPromoter
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.DataContext
+import java.util.*
+
+class ToggleDocTranslationPromoter : ActionPromoter {
+    override fun promote(actions: MutableList<AnAction>, context: DataContext?): MutableList<AnAction> {
+        val newList: MutableList<AnAction> = ArrayList(actions)
+        val comparator = Comparator.comparingInt { action: AnAction ->
+            when (action) {
+                is ToggleQuickDocTranslationAction -> 0
+                else -> 1
+            }
+        }
+        newList.sortWith(comparator)
+        return newList
+    }
+}
