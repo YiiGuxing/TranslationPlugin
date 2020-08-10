8c63449d227dff8c9b7f1b7c813ed837ee27dd04
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-08-10 17:53:53 +0300

    don't use ToolWindowManager.invokeLater,
    it leaks lambda in case of plugin unloading
    
    (cherry picked from commit bf70fa1cce57b66d97877446806ee0e349520821)

--- src/main/kotlin/cn/yiiguxing/plugin/translate/activity/BaseStartupActivity.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/activity/BaseStartupActivity.kt
@@ -3,7 +3,6 @@ package cn.yiiguxing.plugin.translate.activity
 import cn.yiiguxing.plugin.translate.util.Application
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import com.intellij.openapi.wm.ToolWindowManager
 
 abstract class BaseStartupActivity(private val runOnlyOnce: Boolean = false) : StartupActivity {
 
@@ -16,28 +15,11 @@ abstract class BaseStartupActivity(private val runOnlyOnce: Boolean = false) : S
 
         veryFirstProjectOpening = false
         if (onBeforeRunActivity(project)) {
-            runLater(project, 3) { onRunActivity(project) }
+            onRunActivity(project)
         }
     }
 
     protected open fun onBeforeRunActivity(project: Project): Boolean = true
 
     protected abstract fun onRunActivity(project: Project)
-
-    private companion object {
-        fun runLater(project: Project, delayCount: Int, action: () -> Unit) {
-            if (project.isDisposed) {
-                return
-            }
-
-            if (delayCount > 0) {
-                ToolWindowManager.getInstance(project).invokeLater(
-                    Runnable { runLater(project, delayCount - 1, action) }
-                )
-            } else {
-                action()
-            }
-        }
-    }
-
 }
