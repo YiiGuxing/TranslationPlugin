2dffa5035c5740801f429b526ea16069776b18ad
Yii.Guxing <yii.guxing@gmail.com>
2021-03-25 20:52:40 +0800

    theme

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     var searchParams = new URLSearchParams(window.location.search);
-    if (searchParams.get('editor')) {
+    if (searchParams.get('editor') === "true") {
         var classList = document.querySelector("body").classList;
         classList.add("editor");
-        if (searchParams.get('dark')) {
+        if (searchParams.get('dark') === "true") {
             classList.add("dark");
         }
     }
