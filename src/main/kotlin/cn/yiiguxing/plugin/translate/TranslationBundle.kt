c3fbbe8876f49b15bc2152e779f350da2e5d12f7
Yii.Guxing <yii.guxing@gmail.com>
2021-05-06 21:29:18 +0800

    fix no such method error: 'java.util.Locale com.intellij.DynamicBundle.getLocale()'
    fix #747

--- src/main/kotlin/cn/yiiguxing/plugin/translate/TranslationBundle.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/TranslationBundle.kt
@@ -1,47 +1,10 @@
 package cn.yiiguxing.plugin.translate
 
-import com.intellij.AbstractBundle
-import com.intellij.DynamicBundle
 import org.jetbrains.annotations.PropertyKey
-import java.util.*
 
 const val BUNDLE = "messages.TranslationBundle"
 
-object TranslationBundle : AbstractBundle(BUNDLE) {
-
-    private val adaptedControl = ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES)
-
-    private val adaptedBundle: AbstractBundle? by lazy {
-        val dynamicLocale = DynamicBundle.getLocale()
-        if (dynamicLocale.toLanguageTag() == Locale.ENGLISH.toLanguageTag()) {
-            object : AbstractBundle(BUNDLE) {
-                override fun findBundle(
-                    pathToBundle: String,
-                    loader: ClassLoader,
-                    control: ResourceBundle.Control
-                ): ResourceBundle {
-                    val dynamicBundle = ResourceBundle.getBundle(pathToBundle, dynamicLocale, loader, adaptedControl)
-                    return dynamicBundle ?: super.findBundle(pathToBundle, loader, control)
-                }
-            }
-        } else null
-    }
-
-    override fun findBundle(
-        pathToBundle: String,
-        loader: ClassLoader,
-        control: ResourceBundle.Control
-    ): ResourceBundle {
-        val dynamicLocale = DynamicBundle.getLocale()
-        val dynamicBundle = ResourceBundle.getBundle(pathToBundle, dynamicLocale, loader, control)
-        return dynamicBundle ?: super.findBundle(pathToBundle, loader, control)
-    }
-
-    fun getAdaptedMessage(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
-        return adaptedBundle?.getMessage(key, *params) ?: getMessage(key, *params)
-    }
-
-}
+object TranslationBundle : MyDynamicBundle(BUNDLE)
 
 fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
     return TranslationBundle.getMessage(key, *params)
--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/Languages.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/Languages.kt
@@ -1,6 +1,6 @@
 package cn.yiiguxing.plugin.translate.trans
 
-import com.intellij.DynamicBundle
+import cn.yiiguxing.plugin.translate.MyDynamicBundle
 import com.intellij.util.xmlb.annotations.Attribute
 import com.intellij.util.xmlb.annotations.Tag
 import org.jetbrains.annotations.PropertyKey
@@ -8,7 +8,7 @@ import java.util.*
 
 private const val LANGUAGE_BUNDLE = "messages.LanguageBundle"
 
-private object LanguageBundle : DynamicBundle(LANGUAGE_BUNDLE)
+private object LanguageBundle : MyDynamicBundle(LANGUAGE_BUNDLE)
 
 
 @Tag("language-pair")
