579b3c3f25f396bbac13219600df4969b424925c
Yii.Guxing <yii.guxing@gmail.com>
2021-07-20 23:31:18 +0800

    migrate .translation directory to $XDG_DATA_HOME
    issue: #788

--- src/main/kotlin/cn/yiiguxing/plugin/translate/Constants.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/Constants.kt
@@ -5,11 +5,23 @@
 
 package cn.yiiguxing.plugin.translate
 
+import com.intellij.openapi.util.SystemInfo
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
 private val USER_HOME_PATH = System.getProperty("user.home")
-val TRANSLATION_DIRECTORY: Path = Paths.get(USER_HOME_PATH, ".translation")
+private val DEFAULT_TRANSLATION_DIRECTORY = Paths.get(USER_HOME_PATH, ".translation")
+
+val TRANSLATION_DIRECTORY: Path =
+    if (SystemInfo.isLinux && !Files.exists(DEFAULT_TRANSLATION_DIRECTORY)) {
+        System.getenv("XDG_DATA_HOME")
+            ?.takeIf { it.isNotEmpty() }
+            ?.let { Paths.get(it, ".translation") }
+            ?: DEFAULT_TRANSLATION_DIRECTORY
+    } else {
+        DEFAULT_TRANSLATION_DIRECTORY
+    }
 
 const val DEFAULT_NOTIFICATION_GROUP_ID = "Translation Plugin"
 const val UPDATE_NOTIFICATION_GROUP_ID = "Translation Plugin Updates"
@@ -19,7 +31,7 @@ const val STORAGE_NAME = "yiiguxing.translation.xml"
 const val GITHUB_URL = "https://github.com/YiiGuxing/TranslationPlugin"
 const val NEW_ISSUES_URL = "https://github.com/YiiGuxing/TranslationPlugin/issues/new/choose"
 const val OPEN_COLLECTIVE_URL = "https://opencollective.com/translation-plugin"
-const val SUPPORT_PATRONS_URL = "http://yiiguxing.github.io/TranslationPlugin/support.html#patrons"
+const val SUPPORT_PATRONS_URL = "https://yiiguxing.github.io/TranslationPlugin/support.html#patrons"
 const val SUPPORT_SHARE_URL = "https://plugins.jetbrains.com/plugin/8579-translation"
 
 const val HTML_DESCRIPTION_SETTINGS = "#SETTINGS"
