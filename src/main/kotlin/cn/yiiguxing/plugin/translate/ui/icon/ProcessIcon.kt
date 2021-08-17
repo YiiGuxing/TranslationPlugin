20c5d985a6addc4200e74458adae59cbe49e7bd5
Yii.Guxing <yii.guxing@gmail.com>
2021-08-18 01:11:21 +0800

    fix process icon size

--- src/main/kotlin/cn/yiiguxing/plugin/translate/ui/icon/ProcessIcon.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/ui/icon/ProcessIcon.kt
@@ -1,11 +1,19 @@
 package cn.yiiguxing.plugin.translate.ui.icon
 
 import com.intellij.util.ui.AnimatedIcon
+import com.intellij.util.ui.JBDimension
 import icons.Icons
+import java.awt.Dimension
 
 internal class ProcessIcon : AnimatedIcon(
     "Querying Process", ICONS, STEP_PASSIVE, CYCLE_LENGTH
 ) {
+
+    override fun calcPreferredSize(): Dimension {
+        // 在高清屏幕上图标显示不全，把尺寸给加大一点点
+        return JBDimension(33, 33)
+    }
+
     companion object {
         private const val CYCLE_LENGTH = 400
 
