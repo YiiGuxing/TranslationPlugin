1abc098ef0b5a6b67ce50e2b6ab3e174dfb632d2
Yii.Guxing <yii.guxing@gmail.com>
2021-09-03 23:46:29 +0800

    add http log

--- src/main/kotlin/cn/yiiguxing/plugin/translate/util/Http.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/util/Http.kt
@@ -1,12 +1,16 @@
 package cn.yiiguxing.plugin.translate.util
 
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.util.io.HttpRequests
 import com.intellij.util.io.RequestBuilder
 
 object Http {
 
+    private val logger = Logger.getInstance(Http::class.java)
+
     fun postDataFrom(url: String, vararg dataFrom: Pair<String, String>, init: RequestBuilder.() -> Unit = {}): String {
         val data = dataFrom.joinToString("&") { (key, value) -> "$key=${value.urlEncode()}" }
+        logger.d("POST ==> $url\n\t|==> $data")
         return HttpRequests.post(url, "application/x-www-form-urlencoded")
             .apply(init)
             .connect {
