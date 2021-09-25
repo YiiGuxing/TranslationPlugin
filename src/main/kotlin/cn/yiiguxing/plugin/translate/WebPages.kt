ee6f63b8e7452cdad3cabdb6d6a1e35ed000c98d
Yii.Guxing <yii.guxing@gmail.com>
2021-09-25 23:35:38 +0800

    add gitee.io site for web pages

--- src/main/kotlin/cn/yiiguxing/plugin/translate/WebPages.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/WebPages.kt
@@ -4,16 +4,23 @@ import java.util.*
 
 object WebPages {
 
-    private const val BASE_URL = "https://yiiguxing.github.io/TranslationPlugin"
+    private const val BASE_URL_GITHUB = "https://yiiguxing.github.io/TranslationPlugin"
+    private const val BASE_URL_GITEE = "https://yiiguxing.gitee.io/translation-plugin"
 
     fun get(vararg path: String, locale: Locale = Locale.getDefault()): String {
+        val baseUrl =
+            if (locale.language == Locale.CHINESE.language && (locale.country == "" || locale.country == "CN")) {
+                BASE_URL_GITEE
+            } else {
+                BASE_URL_GITHUB
+            }
         val langPath = when (locale.language) {
             Locale.CHINESE.language -> ""
             Locale.JAPANESE.language -> "/ja"
             Locale.KOREAN.language -> "/ko"
             else -> "/en"
         }
-        return "$BASE_URL$langPath/${path.joinToString("/")}"
+        return "$baseUrl$langPath/${path.joinToString("/")}"
     }
 
     fun getStarted(locale: Locale = Locale.getDefault()): String = get("start.html", locale = locale)
