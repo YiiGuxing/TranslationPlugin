aa78c529a4be88392820b833df13a2c2994a289b
Yii.Guxing <yii.guxing@gmail.com>
2021-09-28 20:50:40 +0800

    add qodana support

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.1.6"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.0"
+    // Gradle Qodana Plugin
+    id("org.jetbrains.qodana") version "0.1.13"
 }
 
 val fullPluginVersion = properties("pluginVersion").let { pluginVersion ->
@@ -65,6 +67,14 @@ changelog {
     headerParserRegex.set(Regex("v\\d(\\.\\d+)+"))
     keepUnreleasedSection.set(false)
     groups.set(emptyList())
+}
+
+// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
+qodana {
+    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
+    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
+    saveReport.set(true)
+    showReport.set(System.getenv("QODANA_SHOW_REPORT").toBoolean())
 }
 
 java {
