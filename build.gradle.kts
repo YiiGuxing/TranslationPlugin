b5d2d719620b391dc9edc0570e1e8a92eef39f61
Yii.Guxing <yii.guxing@gmail.com>
2021-09-29 23:12:59 +0800

    build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,6 +83,7 @@ java {
 
 tasks {
     runIde {
+        autoReloadPlugins.set(false)
         systemProperties["idea.auto.reload.plugins"] = false
     }
 
@@ -103,6 +104,15 @@ tasks {
 
     runPluginVerifier {
         ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+    }
+
+    publishPlugin {
+        dependsOn("patchChangelog")
+        token.set(System.getenv("PUBLISH_TOKEN"))
+        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        // channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 
     wrapper {
