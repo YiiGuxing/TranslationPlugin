32491eed77307b7bda3ceb2bd522288b5f3d92c5
Yii.Guxing <yii.guxing@gmail.com>
2021-11-18 09:48:01 +0800

    removed updateSinceUntilBuild.set(true) from gradle configuration to use default value

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,6 @@ intellij {
     pluginName.set(properties("pluginName"))
     version.set(properties("platformVersion"))
     type.set(properties("platformType"))
-    updateSinceUntilBuild.set(true)
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
