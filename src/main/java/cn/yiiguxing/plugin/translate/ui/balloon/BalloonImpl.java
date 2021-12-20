9e227446b9fdbf82e83600ec0a10d4e167417616
Yii.Guxing <yii.guxing@gmail.com>
2021-12-20 09:49:29 +0800

    clean up

--- src/main/java/cn/yiiguxing/plugin/translate/ui/balloon/BalloonImpl.java
+++ src/main/java/cn/yiiguxing/plugin/translate/ui/balloon/BalloonImpl.java
@@ -11,6 +11,8 @@ import com.intellij.ide.RemoteDesktopService;
 import com.intellij.ide.ui.PopupLocationTracker;
 import com.intellij.ide.ui.ScreenAreaConsumer;
 import com.intellij.openapi.MnemonicHelper;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ex.AnActionListener;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
@@ -632,9 +634,12 @@ public final class BalloonImpl implements Balloon, IdeTooltip.Ui, ScreenAreaCons
 
         if (ApplicationManager.getApplication() != null) {
             ApplicationManager.getApplication().getMessageBus().connect(this)
-                    .subscribe(AnActionListener.TOPIC, (AnActionListenerAdapter) (action, event) -> {
-                        if (myHideOnAction && !(action instanceof HintManagerImpl.ActionToIgnore)) {
-                            hide();
+                    .subscribe(AnActionListener.TOPIC,new AnActionListener(){
+                        @Override
+                        public void beforeActionPerformed(@NotNull AnAction action, @NotNull AnActionEvent event) {
+                            if (myHideOnAction && !(action instanceof HintManagerImpl.ActionToIgnore)) {
+                                hide();
+                            }
                         }
                     });
         }
