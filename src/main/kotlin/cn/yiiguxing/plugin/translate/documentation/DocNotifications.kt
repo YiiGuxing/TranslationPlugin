b751e1db2f5c410dec0097a3814ebfd0f0e7e4d8
Yii.Guxing <yii.guxing@gmail.com>
2022-03-04 18:54:21 +0800

    remove `DisableAutoDocTranslationAction` from documentation notifications

--- src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/DocNotifications.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/DocNotifications.kt
@@ -2,10 +2,6 @@ package cn.yiiguxing.plugin.translate.documentation
 
 import cn.yiiguxing.plugin.translate.message
 import cn.yiiguxing.plugin.translate.trans.TranslationNotifications
-import cn.yiiguxing.plugin.translate.util.Settings
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationAction
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
 
 object DocNotifications {
@@ -15,15 +11,8 @@ object DocNotifications {
             project,
             message("translate.documentation.notification.title"),
             message("translate.documentation.error"),
-            e,
-            DisableAutoDocTranslationAction()
+            e
         )
     }
 
-    private class DisableAutoDocTranslationAction : NotificationAction(message("translate.documentation.disable")) {
-        override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-            Settings.translateDocumentation = false
-            notification.expire()
-        }
-    }
 }
