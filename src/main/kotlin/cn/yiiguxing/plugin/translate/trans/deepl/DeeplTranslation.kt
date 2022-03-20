ed833dd317219dc45bea377da0cb910f2a83e357
Naoyuki Kodama <mmaga@dstribe.net>
2022-03-20 20:20:07 +0900

    feat: Delete unnecessary processes

--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/deepl/DeeplTranslation.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/deepl/DeeplTranslation.kt
@@ -3,7 +3,6 @@ package cn.yiiguxing.plugin.translate.trans.deepl
 import cn.yiiguxing.plugin.translate.trans.Lang
 import cn.yiiguxing.plugin.translate.trans.Translation
 import cn.yiiguxing.plugin.translate.trans.TranslationAdapter
-import cn.yiiguxing.plugin.translate.util.Settings
 import com.google.gson.annotations.SerializedName
 
 data class DeeplTranslations (
@@ -33,46 +32,4 @@ data class DeeplTranslation(
     val srcLanguage: String,
     @SerializedName("text")
     val text: String? = null
-)
-
-//@Suppress("MemberVisibilityCanBePrivate", "SpellCheckingInspection")
-//data class DeeplTranslation(
-//    @SerializedName("error_code")
-//    val code: Int = 0,
-//    @SerializedName("detected_source_language")
-//    val srcLanguage: String? = null,
-//    @SerializedName("text")
-//    val text: String? = null
-//) : TranslationAdapter {
-//
-//    val isSuccessful get() = code == 0 || code == 52000
-//
-//    override fun toTranslation(): Translation {
-//        check(isSuccessful) { "Cannot convert to Translation: errorCode=$code" }
-//        check(srcLanguage != null) { "Cannot convert to Translation: srcLanguage=null" }
-//        check(text != null) { "Cannot convert to Translation: trans=null" }
-//
-//        val srcLang = Lang.fromDeeplLanguageCode(srcLanguage)
-//        val transLang = Lang.fromDeeplLanguageCode(Settings.deeplTranslateSettings.primaryLanguage.code)
-//        val original = StringBuilder()
-//        val translation = StringBuilder()
-//
-////        trans.forEachIndexed { index, (src, dst) ->
-////            if (index > 0) {
-////                original.append('\n')
-////                translation.append('\n')
-////            }
-////            original.append(src)
-////            translation.append(dst)
-////        }
-////
-//        return Translation(original.toString(), translation.toString(), srcLang, transLang, listOf(srcLang))
-//    }
-//}
-
-data class BTrans(
-    @SerializedName("src")
-    val src: String,
-    @SerializedName("dst")
-    val dst: String
 )
--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/deepl/DeeplTranslator.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/deepl/DeeplTranslator.kt
@@ -2,17 +2,13 @@
 
 package cn.yiiguxing.plugin.translate.trans.deepl
 
-import cn.yiiguxing.plugin.translate.message
 import cn.yiiguxing.plugin.translate.trans.*
 import cn.yiiguxing.plugin.translate.ui.settings.TranslationEngine.DEEPL
 import cn.yiiguxing.plugin.translate.util.Http
 import cn.yiiguxing.plugin.translate.util.Settings
 import cn.yiiguxing.plugin.translate.util.i
-import cn.yiiguxing.plugin.translate.util.md5
 import com.google.gson.Gson
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.diagnostic.Logger
-import java.util.*
 import javax.swing.Icon
 
 /**
@@ -22,7 +18,6 @@ object DeeplTranslator : AbstractTranslator() {
 
     private const val DEEPL_FREE_TRANSLATE_API_URL = "https://api-free.deepl.com/v2/translate"
     private const val DEEPL_PRO_TRANSLATE_API_URL = "https://api.deepl.com/v2/translate"
-    private const val DEEPL_PRODUCT_URL = "https://www.deepl.com/translator"
 
 
     /** 通用版支持的语言列表 */
@@ -103,21 +98,6 @@ object DeeplTranslator : AbstractTranslator() {
         .toMutableList()
         .apply { add(0, Lang.AUTO) }
     override val supportedTargetLanguages: List<Lang> = SUPPORTED_TARGET_LANGUAGES
-
-    private val errorMessageMap: Map<Int, String> by lazy {
-        mapOf(
-            400 to message("error.bad.request"), // Bad request. Please check error message and your parameters.
-            403 to message("error.invalidAccount"), // Authorization failed. Please supply a valid auth_key parameter.
-            404 to message("error.request.not.found"), // The requested resource could not be found.
-            413 to message("error.text.too.long"), // The request size exceeds the limit.
-            414 to message("error.request.uri.too.long"), // The request URL is too long. You can avoid this error by using a POST request instead of a GET request, and sending the parameters in the HTTP body.
-            429 to message("error.too.many.requests"), // Too many requests. Please wait and resend your request.
-            456 to message("error.access.limited"), // Quota exceeded. The character limit has been reached.
-            503 to message("error.deepl.service.is.down"), // Resource currently unavailable. Try again later.
-            529 to message("error.too.many.requests"), // Too many requests. Please wait and resend your request.
-            500 to message("error.systemError"), // Internal error
-        )
-    }
 
     override fun checkConfiguration(force: Boolean): Boolean {
         if (force || Settings.deeplTranslateSettings.let { it.appId.isEmpty() || it.getAppKey().isEmpty() }) {
