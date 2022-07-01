b1a658e0672d3063a02819dd2bd05c065f482230
Yii.Guxing <yii.guxing@gmail.com>
2022-07-01 09:48:01 +0800

    Extract version constant

--- src/main/kotlin/cn/yiiguxing/plugin/translate/update/Version.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/update/Version.kt
@@ -8,7 +8,7 @@ import kotlin.math.min
 /**
  * Semantic versioning -> [semver.org](https://semver.org)
  */
-class Version constructor(val version: String = "0.0.0") : Comparable<Version> {
+class Version(val version: String = INITIAL_VERSION) : Comparable<Version> {
 
     /** The major version number. */
     val major: Int
@@ -132,6 +132,11 @@ class Version constructor(val version: String = "0.0.0") : Comparable<Version> {
     }
 
     companion object {
+        /**
+         * The initial version: `0.0.0`
+         */
+        const val INITIAL_VERSION = "0.0.0"
+
         private const val MAJOR = "major"
         private const val MINOR = "minor"
         private const val PATCH = "patch"
