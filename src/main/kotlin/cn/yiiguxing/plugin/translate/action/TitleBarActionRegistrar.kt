ce2bdb1ae5ff0adb0e74cc06e0eacc57e2a37b5f
Yii.Guxing <yii.guxing@gmail.com>
2022-09-04 01:46:00 +0800

    Prevent IDE startup failure due to failed registration action

--- src/main/kotlin/cn/yiiguxing/plugin/translate/action/TitleBarActionRegistrar.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/action/TitleBarActionRegistrar.kt
@@ -2,6 +2,7 @@ package cn.yiiguxing.plugin.translate.action
 
 import cn.yiiguxing.plugin.translate.util.IdeVersion
 import cn.yiiguxing.plugin.translate.util.Plugin
+import cn.yiiguxing.plugin.translate.util.w
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.plugins.DynamicPluginListener
 import com.intellij.ide.plugins.IdeaPluginDescriptor
@@ -9,6 +10,7 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.Constraints
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl
+import com.intellij.openapi.diagnostic.Logger
 
 class TitleBarActionRegistrar : AppLifecycleListener, DynamicPluginListener {
 
@@ -25,17 +27,23 @@ class TitleBarActionRegistrar : AppLifecycleListener, DynamicPluginListener {
     }
 
     private fun registerAction() {
-        val actionManager = ActionManager.getInstance() as ActionManagerImpl
-        val group = actionManager.getAction(TITLE_BAR_ACTION_GROUP_ID) as? DefaultActionGroup ?: return
-        val action = actionManager.getAction(TRANSLATION_TITLE_BAR_ACTION_ID) ?: return
-        if (!group.containsAction(action)) {
-            actionManager.addToGroup(group, action, Constraints.FIRST)
+        try {
+            val actionManager = ActionManager.getInstance() as ActionManagerImpl
+            val group = actionManager.getAction(TITLE_BAR_ACTION_GROUP_ID) as? DefaultActionGroup ?: return
+            val action = actionManager.getAction(TRANSLATION_TITLE_BAR_ACTION_ID) ?: return
+            if (!group.containsAction(action)) {
+                actionManager.addToGroup(group, action, Constraints.FIRST)
+            }
+        } catch (e: Throwable) {
+            LOG.w(e)
         }
     }
 
     companion object {
         private const val TRANSLATION_TITLE_BAR_ACTION_ID = "TranslationTitleBar"
         private const val TITLE_BAR_ACTION_GROUP_ID = "ExperimentalToolbarActions"
+
+        private val LOG: Logger = Logger.getInstance(TitleBarActionRegistrar::class.java)
     }
 
 }
