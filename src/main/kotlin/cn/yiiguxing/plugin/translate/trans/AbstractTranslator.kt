01e8a6ce308c5b733c074da1118362e8d9db0aec
Yii.Guxing <yii.guxing@gmail.com>
2022-10-08 23:25:40 +0800

    Extract IO exception message method

--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/AbstractTranslator.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/AbstractTranslator.kt
@@ -1,14 +1,10 @@
 package cn.yiiguxing.plugin.translate.trans
 
 import cn.yiiguxing.plugin.translate.message
+import cn.yiiguxing.plugin.translate.util.getCommonMessage
 import com.intellij.util.io.HttpRequests
 import io.netty.handler.codec.http.HttpResponseStatus
 import java.io.IOException
-import java.net.ConnectException
-import java.net.SocketException
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
-import javax.net.ssl.SSLException
 
 /**
  * Provides a skeletal implementation of the [Translator] interface.
@@ -35,21 +31,13 @@ abstract class AbstractTranslator : Translator {
     protected open fun createErrorInfo(throwable: Throwable): ErrorInfo? {
         val errorMessage = when (throwable) {
             is UnsupportedLanguageException -> message("error.unsupportedLanguage", throwable.lang.langName)
-            is ConnectException, is UnknownHostException -> message("error.network.connection")
-            is SocketException, is SSLException -> message("error.network")
-            is SocketTimeoutException -> message("error.network.timeout")
             is ContentLengthLimitException -> message("error.text.too.long")
             is HttpRequests.HttpStatusException -> when (throwable.statusCode) {
-                HttpResponseStatus.TOO_MANY_REQUESTS.code() -> message("error.too.many.requests")
-                HttpResponseStatus.FORBIDDEN.code() -> message("error.invalidAccount")
-                HttpResponseStatus.BAD_REQUEST.code() -> message("error.bad.request")
                 HttpResponseStatus.REQUEST_ENTITY_TOO_LARGE.code() -> message("error.text.too.long")
-                HttpResponseStatus.SERVICE_UNAVAILABLE.code() -> message("error.service.unavailable")
-                HttpResponseStatus.INTERNAL_SERVER_ERROR.code() -> message("error.systemError")
-                else -> HttpResponseStatus.valueOf(throwable.statusCode).reasonPhrase()
+                else -> throwable.getCommonMessage()
             }
 
-            is IOException -> message("error.io.exception", throwable.message ?: "")
+            is IOException -> throwable.getCommonMessage()
             else -> return null
         }
 
