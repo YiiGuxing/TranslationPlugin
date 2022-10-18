df66b41503f2d4a0491c698375934dad367b6d2d
Yii.Guxing <yii.guxing@gmail.com>
2022-10-18 15:07:57 +0800

    Optimize word book tool window

--- src/main/kotlin/cn/yiiguxing/plugin/translate/wordbook/WordBookToolWindowFactory.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/wordbook/WordBookToolWindowFactory.kt
@@ -1,13 +1,10 @@
 package cn.yiiguxing.plugin.translate.wordbook
 
-import cn.yiiguxing.plugin.translate.util.Application
-import cn.yiiguxing.plugin.translate.util.checkDispatchThread
-import cn.yiiguxing.plugin.translate.util.invokeLater
+import cn.yiiguxing.plugin.translate.util.*
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.Ref
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ex.ToolWindowEx
@@ -27,62 +24,78 @@ class WordBookToolWindowFactory : ToolWindowFactory, DumbAware {
     }
 
     override fun init(toolWindow: ToolWindow) {
-        val toolWindowRef: Ref<ToolWindow?> = Ref.create(toolWindow)
-        Disposer.register(toolWindow.disposable) { toolWindowRef.set(null) }
-
+        val toolWindowRef = DisposableRef.create(toolWindow.disposable, toolWindow)
         val project = (toolWindow as ToolWindowEx).project
+
+        val isStripeButtonShown = toolWindow.isShowStripeButton
+
         val messageBusConnection = project.messageBus.connect(toolWindow.disposable)
         messageBusConnection.subscribe(RequireWordBookListener.TOPIC, object : RequireWordBookListener {
             override fun onRequire() {
                 toolWindowRef.get()?.runIfSurvive {
-                    if (isAvailable) {
-                        return@runIfSurvive
-                    }
                     setAvailable(true) {
-                        isShowStripeButton = true
+                        if (isStripeButtonShown) {
+                            showStripeButton()
+                        } else {
+                            isShowStripeButton = true
+                        }
                         show()
                     }
                 }
             }
         })
+
+        if (!isStripeButtonShown) {
+            return
+        }
+
+        // 这里只是修改了状态的复本，这会导致真实的状态和已存储的状态不一致。见：`ToolWindow.showStripeButton()`
+        toolWindow.isShowStripeButton = false
+
         messageBusConnection.subscribe(WordBookListener.TOPIC, object : WordBookListener {
             override fun onWordsAdded(service: WordBookService, words: List<WordBookItem>) {
-                toolWindowRef.get()?.runIfSurvive { isAvailable = true }
-            }
-
-            override fun onWordsRemoved(service: WordBookService, wordIds: List<Long>) {
-                Application.executeOnPooledThread {
-                    val available = WordBookService.instance.hasAnyWords()
-                    toolWindowRef.get()?.runIfSurvive { isAvailable = available }
-                }
+                toolWindowRef.showStripeButton()
             }
 
             override fun onStoragePathChanged(service: WordBookService) {
-                updateAvailable(toolWindowRef)
+                toolWindowRef.updateVisible()
             }
         })
 
         val disposable = Disposer.newDisposable(toolWindow.disposable, "Wordbook tool window availability state")
         WordBookService.instance.stateBinding.observe(disposable) { state, _ ->
             if (state == WordBookState.RUNNING) {
+                toolWindowRef.updateVisible()
                 Disposer.dispose(disposable)
-                updateAvailable(toolWindowRef)
             }
         }
         if (WordBookService.instance.isInitialized) {
+            toolWindowRef.updateVisible()
             Disposer.dispose(disposable)
-            updateAvailable(toolWindowRef)
         }
     }
 
-    private fun updateAvailable(toolWindowRef: Ref<ToolWindow?>) {
-        runAsync { with(WordBookService.instance) { isInitialized && hasAnyWords() } }
-            .onSuccess { available ->
-                toolWindowRef.get()?.runIfSurvive { isAvailable = available }
+    private fun ToolWindow.showStripeButton() {
+        // 由于在`init(ToolWindow)`中设置`isShowStripeButton`为`false`时，只修改了状态的复本，导致真实的状态和记录
+        // 的状态（可能为`true`）不同步，直接设置为`true`可能无效，所以这里需要设置为`false`先同步一下状态。
+        isShowStripeButton = false
+        isShowStripeButton = true
+    }
+
+    private fun DisposableRef<ToolWindow>.showStripeButton() {
+        get()?.runIfSurvive { showStripeButton() }
+    }
+
+    private fun DisposableRef<ToolWindow>.updateVisible() {
+        runAsync { with(WordBookService.instance) { isInitialized && hasWords() } }
+            .successOnUiThread(this, ModalityState.NON_MODAL) { toolWindow, available ->
+                if (available) {
+                    toolWindow.showStripeButton()
+                }
             }
     }
 
-    override fun shouldBeAvailable(project: Project): Boolean = false
+    override fun shouldBeAvailable(project: Project): Boolean = true
 
     companion object {
         const val TOOL_WINDOW_ID = "Word Book"
