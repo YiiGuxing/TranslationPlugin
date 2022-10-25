269ff1c38a7336cfbfa95f9d141c11643ec665ee
Yii.Guxing <yii.guxing@gmail.com>
2022-10-25 16:27:07 +0800

    Re-add check google network startup activity

---
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/activity/CheckGoogleNetworkStartupActivity.kt
@@ -0,0 +1,55 @@
+package cn.yiiguxing.plugin.translate.activity
+
+import cn.yiiguxing.plugin.translate.service.TranslationUIManager
+import cn.yiiguxing.plugin.translate.trans.google.TKK
+import cn.yiiguxing.plugin.translate.ui.settings.TranslationEngine
+import cn.yiiguxing.plugin.translate.util.DisposableRef
+import cn.yiiguxing.plugin.translate.util.Notifications
+import cn.yiiguxing.plugin.translate.util.TranslateService
+import cn.yiiguxing.plugin.translate.util.successOnUiThread
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.project.Project
+import org.jetbrains.concurrency.runAsync
+import java.util.*
+
+class CheckGoogleNetworkStartupActivity : BaseStartupActivity() {
+
+    override fun onRunActivity(project: Project) {
+        // 简单判断一下中文环境就可以了...
+        if (Locale.getDefault() != Locale.CHINA ||
+            TranslateService.translator.id != TranslationEngine.GOOGLE.id ||
+            Notifications.isDoNotShowAgain(DO_NOT_NOTIFY_AGAIN_KEY)
+        ) {
+            return
+        }
+
+        val projectRef = DisposableRef.create(TranslationUIManager.disposable(project), project)
+        runAsync { TKK.testConnection() }
+            .successOnUiThread(projectRef, ModalityState.NON_MODAL) { p, res ->
+                if (!p.isDisposed && !res) {
+                    showNotification(p)
+                }
+            }
+    }
+
+    companion object {
+        private const val DO_NOT_NOTIFY_AGAIN_KEY = "check.google.network"
+
+        private const val ANNOUNCEMENT_URL = "https://github.com/YiiGuxing/TranslationPlugin/discussions/2315"
+
+        private fun showNotification(project: Project) {
+            Notifications.showFullContentNotification(
+                "当前翻译引擎似乎不可用？",
+                "2022年10月1日，Google突然停止了Google翻译在中国大陆的业务，不再向中国大陆区域提供翻译服务，" +
+                        "官方给出的理由是“因为使用率低”。这一变化直接不可避免地影响到了插件内置的Google翻译引擎和基于Google翻译的" +
+                        "语音朗读（TTS）功能，导致其无法正常使用。因此建议大家换用其他翻译引擎，有道翻译、百度翻译和阿里翻译" +
+                        "都是很不错的选择。未来开发者会带来更多新的翻译引擎和语音合成（TTS）引擎供大家选择，敬请期待！",
+                project = project,
+            ) {
+                it.addAction(Notifications.BrowseUrlAction("去吐槽", ANNOUNCEMENT_URL))
+                it.addAction(Notifications.DoNotShowAgainAction(DO_NOT_NOTIFY_AGAIN_KEY))
+            }
+        }
+    }
+
+}
