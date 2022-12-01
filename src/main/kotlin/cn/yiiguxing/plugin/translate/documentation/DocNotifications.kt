27a3fa8d5c8d529803c5ecd6d9b765ec06e564ca
Yii.Guxing <yii.guxing@gmail.com>
2022-12-01 18:09:36 +0800

    Translation timeout notifications no longer use singleton notification
    fix: #2584

--- src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/DocNotifications.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/DocNotifications.kt
@@ -3,15 +3,12 @@ package cn.yiiguxing.plugin.translate.documentation
 import cn.yiiguxing.plugin.translate.message
 import cn.yiiguxing.plugin.translate.trans.TranslationNotifications
 import cn.yiiguxing.plugin.translate.util.Notifications
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 
 object DocNotifications {
 
-    private val notificationManager = Notifications.createSingletonNotificationManager(type = NotificationType.WARNING)
-
     fun showTranslationTimeoutWarning(project: Project?) {
-        notificationManager.notify("", message("doc.message.translation.timeout.please.try.again"), project)
+        Notifications.showWarningNotification("", message("doc.message.translation.timeout.please.try.again"), project)
     }
 
     fun showError(e: Throwable, project: Project?) {
