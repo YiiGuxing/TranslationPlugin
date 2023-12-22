4cb30fd2d384f1dcdb7bcbe40b8c66d7c53d159c
Yii.Guxing <yii.guxing@outlook.com>
2023-12-22 22:43:14 +0800

    Add `gpt-3.5-turbo-instruct` model for OpenAi

--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/OpenAIModel.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/OpenAIModel.kt
@@ -8,6 +8,7 @@ package cn.yiiguxing.plugin.translate.trans.openai
 enum class OpenAIModel(val value: String, val modelName: String) {
     GPT_3_5_TURBO("gpt-3.5-turbo", "GPT-3.5-Turbo"),
     GPT_3_5_TURBO_16K("gpt-3.5-turbo-16k", "GPT-3.5-Turbo-16K"),
+    GPT_3_5_TURBO_INSTRUCT("gpt-3.5-turbo-instruct", "GPT-3.5-Turbo-Instruct"),
     GPT_4("gpt-4", "GPT-4"),
     GPT_4_32K("gpt-4-32k", "GPT-4-32K");
 }
