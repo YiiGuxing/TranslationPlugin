fd1adfbae77e0f3c28cdce7b34442c500f10d1db
Yii.Guxing <yii.guxing@outlook.com>
2024-03-26 23:54:47 +0800

    Add context class loader utils

---
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/util/Threads.kt
@@ -0,0 +1,23 @@
+package cn.yiiguxing.plugin.translate.util
+
+import cn.yiiguxing.plugin.translate.TranslationPlugin
+
+/**
+ * Execute the [block] with the specified class loader.
+ */
+inline fun <T> withContextClassLoader(classLoader: ClassLoader, block: () -> T): T {
+    val originalClassLoader = Thread.currentThread().contextClassLoader
+    try {
+        Thread.currentThread().contextClassLoader = classLoader
+        return block()
+    } finally {
+        Thread.currentThread().contextClassLoader = originalClassLoader
+    }
+}
+
+/**
+ * Execute the [block] with the plugin's class loader.
+ */
+inline fun <T> withPluginContextClassLoader(block: () -> T): T {
+    return withContextClassLoader(TranslationPlugin::class.java.classLoader, block)
+}
