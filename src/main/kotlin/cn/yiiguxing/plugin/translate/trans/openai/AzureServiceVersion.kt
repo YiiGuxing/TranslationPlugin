c2c231997e967a48c90ed9f66a8783a48fbb2a78
Yii.Guxing <yii.guxing@outlook.com>
2024-05-11 22:19:45 +0800

    Update azure service API version

--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/AzureServiceVersion.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/AzureServiceVersion.kt
@@ -5,7 +5,7 @@ package cn.yiiguxing.plugin.translate.trans.openai
 enum class AzureServiceVersion(val value: String) {
     V2023_05_15("2023-05-15"),
     V2024_02_01("2024-02-01"),
-    V2024_03_01_PREVIEW("2024-03-01-preview");
+    V2024_04_01_PREVIEW("2024-03-01-preview");
 
     companion object {
         fun previewVersions() = AzureServiceVersion.values().filter {
