a6a5ba01461f681054576272722778db497a4eea
Yii.Guxing <yii.guxing@gmail.com>
2024-05-15 10:07:35 +0800

    OpenAI translator has added support for the GPT-4o model

--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/OpenAiModel.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/OpenAiModel.kt
@@ -9,6 +9,7 @@ enum class OpenAiModel(val value: String, val modelName: String) {
     GPT_3_5_TURBO("gpt-3.5-turbo", "GPT-3.5-Turbo"),
     GPT_3_5_TURBO_1106("gpt-3.5-turbo-1106", "GPT-3.5-Turbo-1106"),
     GPT_4_TURBO("gpt-4-turbo", "GPT-4-Turbo"),
+    GPT_4O("gpt-4o", "GPT-4o"),
     GPT_4("gpt-4", "GPT-4"),
     GPT_4_32K("gpt-4-32k", "GPT-4-32K"),
 
