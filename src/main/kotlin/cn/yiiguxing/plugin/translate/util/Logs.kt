ea0fb9de346abb119dc7f6bc5c68061ff6bb7248
Yii.Guxing <yii.guxing@outlook.com>
2024-07-24 01:00:43 +0800

    Refine logging

--- src/main/kotlin/cn/yiiguxing/plugin/translate/util/Logs.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/util/Logs.kt
@@ -3,30 +3,13 @@
 package cn.yiiguxing.plugin.translate.util
 
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.util.SystemProperties
 
-
-private val isStdout: Boolean = SystemProperties.getBooleanProperty("translation.plugin.log.stdout", false)
-
-
-fun Logger.d(message: String) {
-    if (isStdout) println("DEBUG - $message")
-    debug(message)
-}
-
-fun Logger.i(message: String) {
-    if (isStdout) println("INFO - $message")
-    info(message)
-}
-
+fun Logger.v(message: String) = trace(message)
+fun Logger.d(message: String) = debug(message)
+fun Logger.i(message: String) = info(message)
 fun Logger.w(tr: Throwable) = warn(tr)
-
 fun Logger.w(message: String) = warn(message)
-
 fun Logger.w(message: String, tr: Throwable) = warn(message, tr)
-
 fun Logger.e(message: String) = error(message)
-
 fun Logger.e(message: String, tr: Throwable) = error(message, tr)
-
 fun Logger.e(message: String, tr: Throwable? = null, vararg details: String) = error(message, tr, *details)
