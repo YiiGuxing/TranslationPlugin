e2f75768017675c4d180ef73a2f67b28a3463c01
Yii.Guxing <yii.guxing@outlook.com>
2024-09-20 12:15:44 +0800

    Remove Qodana configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
     id("java") // Java support
     alias(libs.plugins.kotlin) // Kotlin support
     alias(libs.plugins.kover) // Gradle Kover Plugin
-    alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
 }
