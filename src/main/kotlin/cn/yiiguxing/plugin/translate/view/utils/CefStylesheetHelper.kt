749550f285eaf86796783415d6011d3f5d252f28
Yii.Guxing <yii.guxing@gmail.com>
2025-05-07 14:03:08 +0800

    Update base styles in web view

--- src/main/kotlin/cn/yiiguxing/plugin/translate/view/utils/CefStylesheetHelper.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/view/utils/CefStylesheetHelper.kt
@@ -1,41 +1,43 @@
 package cn.yiiguxing.plugin.translate.view.utils
 
-import cn.yiiguxing.plugin.translate.util.alphaBlend
 import cn.yiiguxing.plugin.translate.util.toRGBHex
-import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.ui.JBColor
 
 object CefStylesheetHelper {
+    private val BACKGROUND = JBColor(0xFFFFFF, 0x19191C)
+    private val BASE_COLOR = JBColor(0x19191C, 0xDFE1E6)
+    private val PRIMARY_COLOR = JBColor(0x000000, 0xFFFFFF)
+    private val LINK_COLOR = JBColor(0x0000EE, 0x2196F3)
 
     fun buildBaseStyle(): String {
-        val scheme = EditorColorsManager.getInstance().schemeForCurrentUITheme
-        val background = scheme.defaultBackground
-        val foreground = JBColor(0x19191C, 0xFFFFFF)
-
-        val backgroundHex = background.toRGBHex()
-        val foregroundHex = foreground.toRGBHex()
-
-        val foregroundAlpha = (10..90 step 10).asSequence().map {
-            "--base-color-a$it: " + foreground.alphaBlend(background, it / 100f).toRGBHex() + ";"
+        val baseColorAlpha = (10..90 step 10).asSequence().map {
+            buildAlphaProperty("--base-color", BASE_COLOR, it)
+        }.joinToString("\n                ")
+        val primaryColorAlpha = (10..90 step 10).asSequence().map {
+            buildAlphaProperty("--primary-color", PRIMARY_COLOR, it)
         }.joinToString("\n                ")
 
-        val linkColor = JBColor(0x0269E1, 0x56A8F5).toRGBHex()
+        val backgroundHex = BACKGROUND.toRGBHex()
+        val baseColorHex = BASE_COLOR.toRGBHex()
+        val linkColorHex = LINK_COLOR.toRGBHex()
 
         return """
             :root {
-                --base-color: $foregroundHex;
-                $foregroundAlpha
+                --base-color: $baseColorHex;
+                $baseColorAlpha
+                --primary-color: ${PRIMARY_COLOR.toRGBHex()};
+                $primaryColorAlpha
                 --base-background-color: $backgroundHex;
-                --base-link-color: $linkColor;
+                --base-link-color: $linkColorHex;
             }
             
             body {
-                color: var(--base-color, $foregroundHex);
+                color: var(--base-color, $baseColorHex);
                 background-color: var(--base-background-color, $backgroundHex);
             }
             
             a {
-                color: var(--base-link-color, $linkColor);
+                color: var(--base-link-color, $linkColorHex);
                 text-decoration: none;
             }
             
@@ -43,5 +45,9 @@ object CefStylesheetHelper {
                 text-decoration: underline;
             }
         """.trimIndent()
+    }
+
+    private fun buildAlphaProperty(name: String, color: JBColor, transparency: Int): String {
+        return "$name-a$transparency: rgba(${color.red}, ${color.green}, ${color.blue}, ${transparency / 100f});"
     }
 }
