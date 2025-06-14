5362eb0bd1047f04e68fb6c302f5a82f5b1e921a
Yii.Guxing <yii.guxing@outlook.com>
2025-06-14 12:09:34 +0800

    Implement async translation support in documentation target

--- src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/TranslatableDocumentationTarget.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/TranslatableDocumentationTarget.kt
@@ -1,8 +1,13 @@
 package cn.yiiguxing.plugin.translate.documentation
 
 import com.intellij.model.Pointer
+import com.intellij.platform.backend.documentation.AsyncDocumentation
+import com.intellij.platform.backend.documentation.DocumentationContent
 import com.intellij.platform.backend.documentation.DocumentationResult
 import com.intellij.platform.backend.documentation.DocumentationTarget
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 
 /**
  * A [DocumentationTarget] that supports translation.
@@ -28,8 +33,28 @@ class TranslatableDocumentationTarget(
     }
 
     override fun computeDocumentation(): DocumentationResult? {
-        // TODO: Implement translation logic here
-        return delegate.computeDocumentation()
-    }
+        val originalResult = delegate.computeDocumentation()
+        if (!shouldTranslate || originalResult == null) {
+            return originalResult
+        }
 
+        return DocumentationResult.asyncDocumentation {
+            val documentation = when (originalResult) {
+                is AsyncDocumentation -> originalResult.supplier()
+                else -> originalResult as DocumentationResult.Documentation
+            } ?: return@asyncDocumentation null
+
+            val translatedDocumentationFlow = flow {
+                val translatedDocumentation = withContext(Dispatchers.IO) {
+                    // TODO: 1. Implement translation logic here.
+                    //       2. Return the translated version of the documentation.
+                    "<html><body>Translated Documentation</body></html>"
+                }
+                emit(DocumentationContent.content(translatedDocumentation))
+            }
+
+            // TODO: Append the translating status to the original documentation
+            documentation.updates(translatedDocumentationFlow)
+        }
+    }
 }
