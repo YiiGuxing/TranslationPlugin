6c7a7c0965813b5e45e9ed2f3eda73ba14d93b02
Yii.Guxing <yii.guxing@outlook.com>
2025-08-24 04:14:54 +0800

    Implement inline documentation translating

--- src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/actions/FileInlineDocumentationTranslateActionProvider.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/documentation/actions/FileInlineDocumentationTranslateActionProvider.kt
@@ -1,22 +1,29 @@
 package cn.yiiguxing.plugin.translate.documentation.actions
 
 import cn.yiiguxing.plugin.translate.action.editor
-import cn.yiiguxing.plugin.translate.documentation.Documentations
+import cn.yiiguxing.plugin.translate.documentation.*
 import cn.yiiguxing.plugin.translate.documentation.utils.translateInlineDocumentation
 import cn.yiiguxing.plugin.translate.message
 import cn.yiiguxing.plugin.translate.service.ITPCoroutineService
+import cn.yiiguxing.plugin.translate.trans.TranslateService
+import cn.yiiguxing.plugin.translate.trans.Translator
 import cn.yiiguxing.plugin.translate.ui.notification.banner.EditorBanner
 import cn.yiiguxing.plugin.translate.ui.notification.banner.EditorBannerManager
+import cn.yiiguxing.plugin.translate.util.RateLimiter
+import cn.yiiguxing.plugin.translate.util.findElementAroundOffset
 import com.intellij.codeInsight.actions.ReaderModeSettings
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerEx
 import com.intellij.codeInsight.documentation.render.DocRenderItemManager
 import com.intellij.codeInsight.documentation.render.DocRenderManager
 import com.intellij.ide.ActivityTracker
+import com.intellij.lang.Language
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.application.Experiments
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.readAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.RangeMarker
@@ -25,6 +32,7 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.intellij.psi.PsiDocCommentBase
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import com.intellij.ui.AnimatedIcon
@@ -32,9 +40,12 @@ import icons.TranslationIcons
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.isActive
+import kotlin.time.Duration.Companion.milliseconds
 
 
 private const val TRANSLATION_ITEM_LIMIT = 50
+private const val UPDATE_INTERVAL_MILLIS = 1000
+private const val BUFFER_INITIAL_CAPACITY = 10
 private val FILE_TRANSLATION_JOB_KEY = Key.create<Job>("translation.inlineDocumentation.fileTranslationJob")
 
 
@@ -122,8 +133,7 @@ class FileInlineDocumentationTranslateActionProvider : InspectionWidgetActionPro
         }
 
         private data class RenderItem(val textToRender: String, val range: RangeMarker) {
-            lateinit var translatedText: String
-            var isTranslationError: Boolean = false
+            lateinit var translationInfo: InlineDocTranslationInfo
         }
 
         @Suppress("UnstableApiUsage")
@@ -150,22 +160,32 @@ class FileInlineDocumentationTranslateActionProvider : InspectionWidgetActionPro
             val language = psiFile.language
             editor.translationJob = ITPCoroutineService.projectScope(project)
                 .launch(Dispatchers.IO) {
-                    val result = ArrayList<RenderItem>(renderItems.size)
-                    for (item in renderItems) {
-                        if (!isActive) {
-                            break
-                        }
-                        if (Documentations.isTranslated(item.textToRender)) {
-                            continue
-                        }
+                    val translator = TranslateService.getInstance().translator
+                    val result = ArrayList<RenderItem>(minOf(renderItems.size, BUFFER_INITIAL_CAPACITY))
+                    val rateLimiter = RateLimiter.withInterval(translator.intervalLimit.milliseconds)
+                    var lastUpdateTime = System.currentTimeMillis()
+                    try {
+                        for (item in renderItems) {
+                            if (!isActive || !isAvailable(project, editor)) {
+                                break
+                            }
+                            if (Documentations.isTranslated(item.textToRender)) {
+                                continue
+                            }
 
-                        val (translatedText, hasError) = translateInlineDocumentation(item.textToRender, language)
-                        item.translatedText = translatedText
-                        item.isTranslationError = hasError
-                        result += item
+                            result += item.apply {
+                                translationInfo = translateItem(psiFile, item, rateLimiter, language, translator)
+                            }
+
+                            if (System.currentTimeMillis() - lastUpdateTime >= UPDATE_INTERVAL_MILLIS) {
+                                applyTranslationResult(project, editor, psiFile, result)
+                                result.clear()
+                                lastUpdateTime = System.currentTimeMillis()
+                            }
+                        }
+                    } finally {
+                        applyTranslationResult(project, editor, psiFile, result)
                     }
-
-                    applyTranslationResult(project, editor, psiFile, result)
                 }.apply {
                     invokeOnCompletion {
                         editor.takeUnless { it.isDisposed }?.translationJob = null
@@ -173,7 +193,39 @@ class FileInlineDocumentationTranslateActionProvider : InspectionWidgetActionPro
                 }
         }
 
-        private suspend fun applyTranslationResult(
+        private suspend fun translateItem(
+            psiFile: PsiFile,
+            item: RenderItem,
+            rateLimiter: RateLimiter,
+            language: Language,
+            translator: Translator
+        ): InlineDocTranslationInfo {
+            getTranslationCache(psiFile, item)?.let { cache ->
+                return cache
+            }
+
+            rateLimiter.acquire()
+            return translateInlineDocumentation(
+                text = item.textToRender,
+                language = language,
+                translator = translator
+            ).let { (translatedText, hasError) ->
+                InlineDocTranslationInfo.translated(translatedText, hasError)
+            }
+        }
+
+        private suspend fun getTranslationCache(psiFile: PsiFile, item: RenderItem): InlineDocTranslationInfo? {
+            return readAction {
+                val offset = item.range.takeIf { it.isValid }?.startOffset ?: return@readAction null
+                psiFile.takeIf { it.isValid }
+                    ?.findElementAroundOffset<PsiDocCommentBase>(offset)
+                    ?.let { comment -> getPsiInlineDocumentationTranslationInfo(comment) }
+            }
+                ?.takeIf { !it.isLoading && it.translatedText != null && it.isDisabled }
+                ?.disabled(false)
+        }
+
+        private fun applyTranslationResult(
             project: Project,
             editor: Editor,
             psiFile: PsiFile,
@@ -183,12 +235,19 @@ class FileInlineDocumentationTranslateActionProvider : InspectionWidgetActionPro
                 return
             }
 
-            readAction {
+            ReadAction.run<Throwable> {
                 if (!isAvailable(project, editor) || !psiFile.isValid) {
-                    return@readAction
+                    return@run
                 }
 
-                // TODO: Apply translation result to render items
+                for (item in result) {
+                    val offset = item.range.takeIf { it.isValid }?.startOffset ?: continue
+                    psiFile.findElementAroundOffset<PsiDocCommentBase>(offset)?.let { comment ->
+                        setPsiInlineDocumentationTranslationInfo(comment, item.translationInfo)
+                    }
+                }
+
+                updateRendering(editor, psiFile, ModalityState.any())
             }
         }
 
